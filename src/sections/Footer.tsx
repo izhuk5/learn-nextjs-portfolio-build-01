7c776dd5bb9abe47062616dc9f4ded175401ee15
ihorzhuk <ihorzhuk94@gmail.com>
2025-07-13 13:32:48 +0300

    added animation for Footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,61 +1,103 @@
-import { FC } from "react";
+"use client";
+
+import {FC, MouseEvent, useEffect} from "react";
 import Button from "@/components/Button";
+import useTextRevealAnimation from "@/hooks/useTextRevealAnimation";
+import {useInView} from "motion/react";
 
 const navItems = [
   {
-    href: "#",
-    label: "Home",
+    href: "#about",
+    label: "About",
   },
   {
-    href: "#",
+    href: "#projects",
     label: "Projects",
   },
   {
-    href: "#",
+    href: "#testimonials",
     label: "Testimonials",
   },
   {
-    href: "#",
+    href: "#faqs",
     label: "Faqs",
   },
   {
-    href: "#",
+    href: "#contact",
     label: "Contact",
   },
 ];
 
 const Footer: FC = () => {
+  const { scope, entranceAnimation } = useTextRevealAnimation();
+  const inView = useInView(scope);
+
+  useEffect(() => {
+    if (inView) {
+      entranceAnimation();
+    }
+  }, [inView, entranceAnimation]);
+
+  const handleClickMobileNavItem = (e: MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+
+    const url = new URL(e.currentTarget.href);
+    const hash = url.hash;
+
+    const target = document.querySelector(hash);
+
+    if (!target) return;
+    target.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <footer className="bg-stone-900 text-white" id="contact">
       <div className="container">
         <div className="section">
           <div className="flex items-center gap-3">
-            <div className="size-3 rounded-full bg-green-400"></div>
+            <div className="size-3 rounded-full bg-green-400 animate-pulse"></div>
             <span className="uppercase">One spot available for next month</span>
           </div>
           <div className="grid md:grid-cols-3 md:items-center">
             <div className="md:col-span-2">
-              <h2 className="mt-8 text-4xl font-extralight md:text-7xl lg:text-8xl">
+              <h2 className="mt-8 text-4xl font-extralight md:text-7xl lg:text-8xl" ref={scope}>
                 Enough talk. Let&apos;s make something great together.
               </h2>
               <Button
                 className="mt-8"
                 variant="secondary"
                 iconAfter={
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="size-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                    />
-                  </svg>
+                  <div className="size-6 overflow-hidden">
+                    <div className="w-12 h-6 flex transition-transform duration-300 group-hover/button:-translate-x-1/2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                        />
+                      </svg>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                        />
+                      </svg>
+                    </div>
+                  </div>
                 }
               >
                 info@alextalor.com
@@ -64,7 +106,7 @@ const Footer: FC = () => {
             <div>
               <nav className="mt-16 flex flex-col gap-8 md:mt-0 md:items-end">
                 {navItems.map(({ href, label }) => (
-                  <a href={href} key={label}>
+                  <a href={href} key={label} onClick={handleClickMobileNavItem}>
                     <Button variant="text" className="text-lg">
                       {label}
                     </Button>
